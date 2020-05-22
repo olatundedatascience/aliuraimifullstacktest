69a4cf418f058dab9dd2155de5f465c576e39ed0
Raimi.Aliu <Raimi.Aliu@FCMB-IT-L16582>
2020-05-22 10:15:31 +0100

    added admin component to react

---
+++ src/Components/AdminComponents.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+
+export default class AdminComponent {
+    constructor(){
+        super()
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
